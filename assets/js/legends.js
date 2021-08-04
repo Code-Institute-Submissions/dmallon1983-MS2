fff43ec185872acf5a2e16d4be22353ae41c8b82
dmallon1983 <D.mallon1983@hotmail.com>
2021-08-04 15:19:42 +0000

    added modal functionality and JS objects

---
+++ assets/js/legends.js
@@ -0,0 +1,228 @@
+// let yashin = {
+//     name: "Lev Yashin",
+//     position: "Goalkeeper",
+//     nationality: "Russian",
+//     caps: "74",
+//     goals: "0",
+//     bio: ""
+//   }
+  
+//   let buffon = {
+//     name: "Gianluigi Buffon",
+//     position: "Goalkeeper",
+//     nationality: "Italian",
+//     caps: "176",
+//     goals: "0",
+//     bio: "Gianluigi Buffon (born 28 January 1978) is an Italian professional footballer who plays as a goalkeeper for Serie B club Parma. He is widely regarded as one of the greatest goalkeepers of all time, and by some as the greatest ever. He is one of the few recorded players to have made over 1,100 professional career appearances. Buffon holds the record for the longest streak without conceding a goal in Serie A history, achieved over twelve league matches; he kept the opposition out for 974 consecutive minutes during the 2015–16 season, achieving the most consecutive clean sheets (10) during that run. Buffon also holds the record for most clean sheets and most appearances in both Serie A and with the Italian national team."
+//   }
+  
+//   let banks = {
+//     name: "Gordon Banks",
+//     position: "Goalkeeper",
+//     nationality: "English",
+//     caps: "73",
+//     goals: "0",
+//     bio: ""
+//   }
+  
+//   let casillas = {
+//     name: "Iker Casillas",
+//     position: "Goalkeeper",
+//     nationality: "Spanish",
+//     caps: "167",
+//     goals: "0",
+//     bio: ""
+//   }
+  
+//   let vanDerSar = {
+//     name: "Edwin Van Der Sar",
+//     position: "Goalkeeper",
+//     nationality: "Dutch",
+//     caps: "130",
+//     goals: "0",
+//     bio: ""
+//   }
+  
+//   let neuer = {
+//     name: "Manuel Neuer",
+//     position: "Goalkeeper",
+//     nationality: "German",
+//     caps: "104",
+//     goals: "0",
+//     bio: ""
+//   }
+  
+//   let cafu = {
+//     name: "Cafu",
+//     position: "Right Back",
+//     nationality: "Brazilian",
+//     caps: "142",
+//     goals: "5",
+//     bio: ""
+//   }
+  
+//   let carlos = {
+//     name: "Roberto Carlos",
+//     position: "Left Back",
+//     nationality: "Brazilian",
+//     caps: "125",
+//     goals: "11",
+//     bio: ""
+//   }
+  
+//   let cole = {
+//     name: "Ashley Cole",
+//     position: "Left Back",
+//     nationality: "English",
+//     caps: "107",
+//     goals: "0",
+//     bio: ""
+//   }
+  
+//   let mcgrain = {
+//     name: "Danny McGrain",
+//     position: "Full Back",
+//     nationality: "Scottish",
+//     caps: "62",
+//     goals: "0",
+//     bio: ""
+//   }
+  
+//   let lahm = {
+//     name: "Philipp Lahm",
+//     position: "Right Back",
+//     nationality: "German",
+//     caps: "113",
+//     goals: "5",
+//     bio: ""
+//   }
+  
+//   let zanetti = {
+//     name: "Javier Zanetti",
+//     position: "Right Back",
+//     nationality: "Argentinian",
+//     caps: "145",
+//     goals: "5",
+//     bio: ""
+//   }
+  
+//   let alves = {
+//     name: "Dani Alves",
+//     position: "Right Back",
+//     nationality: "Brazilian",
+//     caps: "118",
+//     goals: "8",
+//     bio: ""
+//   }
+  
+//   let alberto = {
+//     name: "Carlos Alberto Torres",
+//     position: "Right Back",
+//     nationality: "Brazilian",
+//     caps: "53",
+//     goals: "8",
+//     bio: "Carlos Alberto Torres (17 July 1944 – 25 October 2016), was a Brazilian football player and manager who played as an attacking right-sided full-back or wing-back. A technically gifted defender with good ball skills and defensive capabilities, he is widely regarded as one of the best defenders of all time. He also stood out for his leadership, and was an excellent penalty taker. Nicknamed O Capitão, he captained the Brazil national team to victory in the 1970 World Cup, scoring the fourth goal in the final, considered one of the greatest goals in the history of the tournament. Carlos Alberto was a member of the World Team of the 20th Century, and in 2004 was named by Pelé in the FIFA 100 list of the world's greatest living players. He was an inductee to the Brazilian Football Museum Hall of Fame, and was a member of the U.S. National Soccer Hall of Fame."
+//   }
+
+//   const players = {
+//     buffon: {
+//       name: "Gianluigi Buffon",
+//       position: "Goalkeeper",
+//       nationality: "Italian",
+//       caps: "176",
+//       goals: "0",
+//       bio: "Gianluigi Buffon (born 28 January 1978) is an Italian professional footballer who plays as a goalkeeper for Serie B club Parma. He is widely regarded as one of the greatest goalkeepers of all time, and by some as the greatest ever. He is one of the few recorded players to have made over 1,100 professional career appearances. Buffon holds the record for the longest streak without conceding a goal in Serie A history, achieved over twelve league matches; he kept the opposition out for 974 consecutive minutes during the 2015–16 season, achieving the most consecutive clean sheets (10) during that run. Buffon also holds the record for most clean sheets and most appearances in both Serie A and with the Italian national team."
+//     },
+//     yashin: {
+//       name: "Lev Yashin",
+//       position: "Goalkeeper",
+//       nationality: "Russian",
+//       caps: "74",
+//       goals: "0",
+//       bio: ""
+//     }
+//   }
+
+//   $("li").on("click", function() {
+//     const player = players[$(this).data("player")];
+//     $("#bio").html(player.bio);
+//     $("#dialog-message").attr("title",player.name);
+  
+//     $("#dialog-message").dialog({
+//       modal: true,
+//       buttons: {
+//         Ok: function() {
+//           $(this).dialog("close");
+//         }
+//       }
+//     })
+//   })
+
+let players = {
+    buffon: {
+        name: "Gianluigi Buffon",
+        position: "Goalkeeper",
+        nationality: "Italian",
+        caps: "176",
+        goals: "0",
+        bio: "Gianluigi Buffon (born 28 January 1978) is an Italian professional footballer who plays as a goalkeeper for Serie B club Parma. He is widely regarded as one of the greatest goalkeepers of all time, and by some as the greatest ever. He is one of the few recorded players to have made over 1,100 professional career appearances. Buffon holds the record for the longest streak without conceding a goal in Serie A history, achieved over twelve league matches; he kept the opposition out for 974 consecutive minutes during the 2015–16 season, achieving the most consecutive clean sheets (10) during that run. Buffon also holds the record for most clean sheets and most appearances in both Serie A and with the Italian national team."
+    },
+    yashin: {
+        name: "Lev Yashin",
+        position: "Goalkeeper",
+        nationality: "Russian",
+        caps: "74",
+        goals: "0",
+        bio: "Lev Ivanovich Yashin (22 October 1929 – 20 March 1990), nicknamed the Black Spider or the Black Panther, was a Soviet professional footballer, regarded by many as the greatest goalkeeper in the history of the sport. He was known for his athleticism, positioning, stature, bravery, imposing presence in goal, and acrobatic reflex saves. He was also deputy chairman of the Football Federation of the Soviet Union. Yashin earned status for revolutionising the goalkeeping position by imposing his authority on the entire defence. A vocal presence in goal, he shouted orders at his defenders, came off his line to intercept crosses and also ran out to meet onrushing attackers, done at a time when goalkeepers spent the 90 minutes standing in the goal waiting to be called into action. His performances made an indelible impression on a global audience at the 1958 World Cup, the first to be broadcast internationally. He dressed head to toe in apparent black (in truth very dark blue), thus earning his nickname the 'Black Spider', which enhanced his popularity."
+    },
+    banks: {
+        name: "Gordon Banks",
+        position: "Goalkeeper",
+        nationality: "English",
+        caps: "73",
+        goals: "0",
+        bio: "Gordon Banks OBE (30 December 1937 – 12 February 2019) was an English professional footballer who played as a goalkeeper. He made 679 appearances during a 20-year professional career, and won 73 caps for England, highlighted by starting every game of the nation's 1966 World Cup victory. Banks joined Chesterfield in March 1953, and played for their youth team in the 1956 FA Youth Cup final. He made his first team debut in November 1958, and was sold to Leicester City for £7,000 in July 1959. He played in four cup finals for the club, as they were beaten in the 1961 and 1963 FA Cup finals, before winning the League Cup in 1964 and finishing as finalists in 1965. Despite this success, and his World Cup win in 1966, Banks was dropped by Leicester and sold on to Stoke City for £50,000 in April 1967. In the 1970 World Cup, he made one of the game's great saves to prevent a Pelé goal, but was absent due to illness as England were beaten by West Germany at the quarter-final stage. Banks was Stoke City's goalkeeper in the 1972 League Cup win, the club's only major honour. He was still Stoke's and England's number one when a car crash in October 1972 cost him both the sight in his right eye and, eventually, his professional career. He played two last seasons in the United States for the Fort Lauderdale Strikers in 1977 and 1978, and despite only having vision in one eye, was NASL Goalkeeper of the Year in 1977 after posting the best defensive record in the league. He briefly entered management with Telford United, but left the game in December 1980. Regarded as one of the greatest goalkeepers of all time, Banks was named FWA Footballer of the Year in 1972, and was named FIFA Goalkeeper of the Year on six occasions. The IFFHS named Banks the second-best goalkeeper of the 20th century, after Lev Yashin (1st) and ahead of Dino Zoff (3rd)."
+    },
+    casillas: {
+        name: "Iker Casillas",
+        position: "Goalkeeper",
+        nationality: "Spanish",
+        caps: "167",
+        goals: "0",
+        bio: "Iker Casillas Fernández (born 20 May 1981) is a Spanish retired professional footballer who played as a goalkeeper. Popularly dubbed 'San Iker' ('Saint Iker') for his ability to produce spectacular saves, Casillas is widely regarded to be one of the greatest goalkeepers of all time. He is known for his athleticism, quick reactions and outstanding shot-stopping ability. Having spent the majority of his career at Real Madrid, Casillas is one of the few players to achieve over 1000 professional career matches, and holds the record for the most clean sheets in the UEFA Champions League, as well as for the Spain national team. At club level, Casillas started his career with the Real Madrid youth team, eventually gaining promotion to the main team in 1999, where he became the youngest goalkeeper to play in the final of the Champions League, and to win the title, at 19 years and four days. Casillas became the first choice goalkeeper at Real Madrid, winning two Champions Leagues and La Liga titles in his first three seasons, establishing himself as one of the best goalkeepers in the world. During his highly successful career in Madrid, Casillas won all major club titles, including five La Liga titles, four Supercopa de España titles, two Copa del Rey titles, three UEFA Champions Leagues, two UEFA Super Cups, one Intercontinental Cup and the FIFA Club World Cup. After 25 years with Real Madrid, Casillas joined FC Porto in 2015 on a free transfer, where he won the Primeira Liga and a Supertaça Cândido de Oliveira title. While at Porto, he broke the record for most consecutive seasons played in the UEFA Champions League (20) and amassed the most appearances in the competition (177). He ended his playing career in February 2020, after almost a year on the sidelines following a heart attack he suffered during a training session with Porto in May 2019. With 167 international caps, Casillas has the second most appearances for the Spain national team, making him the joint tenth-most capped footballer of all time. An unused substitute at UEFA Euro 2000, Casillas became the nation's first choice goalkeeper at the 2002 World Cup, and went on to play at UEFA Euro 2004 and the 2006 World Cup. In 2008, he was made captain, and went on to lead the team to the European Championship title that year. Casillas also led Spain to their first World Cup win in 2010, where he kept a joint–record five clean sheets, winning the Yashin Award for the best goalkeeper of the tournament. Spain became the first nation to retain the European Championship, winning the title again in 2012, where he set the record for most consecutive minutes without conceding a goal in the competition (509). In 2014, Casillas became one of four players to represent Spain at four World Cups.In 2008, Casillas placed fourth overall in the Ballon d'Or, and has been selected for the UEFA Team of the Year six consecutive times from 2007 to 2012, a record for a goalkeeper. Casillas was also selected in the FIFPro World XI a record five times for a goalkeeper, and was named as the IFFHS World's Best Goalkeeper a record five times, alongside Gianluigi Buffon and Manuel Neuer. He was named as the second best goalkeeper of the 21st century, behind Buffon, and the second best goalkeeper of the past 25 years by the same organisation."
+    },
+    vanDerSar: {
+        name: "Edwin Van Der Sar",
+        position: "Goalkeeper",
+        nationality: "Dutch",
+        caps: "130",
+        goals: "0",
+        bio: "Edwin van der Sar (born 29 October 1970) is a Dutch football executive and former professional player who is currently the chief executive of AFC Ajax, with whom he began his senior playing career in the early 1990s. He is considered to be a member of the club's golden generation and was part of the Ajax team that won the UEFA Champions League in 1995. A goalkeeper, he left Ajax for Juventus in 1999, where he spent two years before moving to England, first to Fulham and then to Manchester United in 2005. There he won a second Champions League title in 2008, making him one of just eight players at the time to have won the competition with more than one club. He retired as a professional in 2011, but briefly came out of retirement in 2016 to play a match for Dutch amateur team VV Noordwijk, for whom he had previously played as a youth. He played 130 times for the Netherlands national team, and was the nation's most-capped player until 2017, when he was overtaken by Wesley Sneijder. Van der Sar is considered by critics and fellow players as one of the best goalkeepers of all time. He is also one of the most successful footballers ever, having won 26 major trophies in his career, mainly at Ajax and Manchester United. During the 2008–09 season, he set a world record by not conceding a league goal for 1,311 minutes. He is also the oldest player to win the Premier League, having done so in 2011 at the age of 40 years and 205 days. Van der Sar has won several individual awards, including Best European Goalkeeper in 1995 and 2009, and UEFA Club Goalkeeper of the Year in 2009."
+    },
+    neuer: {
+        name: "Manuel Neuer",
+        position: "Goalkeeper",
+        nationality: "German",
+        caps: "104",
+        goals: "0",
+        bio: "Manuel Peter Neuer (born 27 March 1986) is a German professional footballer who plays as a goalkeeper and captains both Bundesliga club Bayern Munich and the Germany national team. He is regarded as one of the greatest goalkeepers in the history of the sport. Neuer has been described as a 'sweeper-keeper' because of his playing style and speed when rushing off his line to anticipate opponents, going out of the goalkeeper box. He was named the best goalkeeper of the decade from 2011 to 2020 by IFFHS.\
+        \
+        \
+        Neuer started his career at Schalke 04 where he won the DFB-Pokal and DFL-Ligapokal. In 2011, he signed for Bayern Munich and has since won 25 trophies, including nine Bundesliga titles and two UEFA Champions League titles in 2013 and 2020, both as part of trebles. In 2014, Neuer finished third in the voting, behind Lionel Messi and Cristiano Ronaldo, for the FIFA Ballon d'Or award. He was awarded the UEFA Goalkeeper of the Year and the IFFHS World's Best Goalkeeper five times each.\
+        \
+        \
+        Neuer was selected as Germany's number one goalkeeper for the 2010 FIFA World Cup in South Africa. During the group stages, he only conceded a single goal. He provided the assist for Miroslav Klose's opening goal against England, winning 4–1. Four years later, Neuer won the 2014 FIFA World Cup with Germany as well as the Golden Glove award for being the best goalkeeper in the tournament."
+    }
+}
+
+
+$("li").on("click", function () {
+    const player = players[$(this).data("player")];
+    $(".modal-body").html(player.bio);
+    $(".modal-goals").html(player.goals);
+    $(".modal-caps").html(player.caps);
+    $(".modal-nation").html(player.nationality);
+    $(".modal-position").html(player.position);
+    $(".modal-title").text(player.name);
+    $('.modal').modal('show')
+})
--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,17 +1,36 @@
-function sendMail (contactForm) {
+function sendMail(contactForm) {
     emailjs.send("gmail", "your_11", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email_address.value,
-        "player_request": contactForm.legend.value,
-        "request_reason": contactForm.reason.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email_address.value,
+            "player_request": contactForm.legend.value,
+            "request_reason": contactForm.reason.value
+        })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;
+}
+
+let legends = ["Lev Yashin", "Gordon Banks", "Gianluigi Buffon", "Manuel Neuer", "Iker Casillas", "Edwin Van Der Sar", "Cafu", "Roberto Carlos", "Ashley Cole", "Gary Neville", "Danny McGrain", "Philip Lahm", "Javier Zanetti", "Dani Alves", "Carlos Alberto", "Franz Beckenbeaur", "Franco Baresi", "Paolo Maldini", "Rio Ferdinand", "Nemanja Vidic", "John Terry", "Billy McNeill", "Virgil Van Dijk", "Fabio Cannavaro", "Xavi", "Andres Iniesta", "Roy Keane", "Zinedine Zidane", "Paul Scholes", "Patrick Viera", "Andrea Pirlo", "Claude Makalele", "Bobby Charlton", "Graeme Souness", "Diego Maradona", "Pele", "Ronaldo", "Cristiano Ronaldo", "Lionel Messi", "Thierry Henry", "Ferenc Puskas", "Ronaldinho", "Robert Lewandowski", "Henrik Larsson"];
+
+let form = document.getElementById("signup-form");
+form.addEventListener('submit', handleSubmit);
+
+// let errorMsg = document.getElementById("errors");
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let legend = document.getElementById("legend");
+    if (legends.includes(legend.value)) {
+        // errorMsg.innerHTML = `${legend.value} is already available for selection. If you would like another legend please try again.`;
+        alert(`${legend.value} is already available for selection. If you would like another legend please try again.`);
+    } else {
+        form.submit();
+        console.log('legends');
+    }
 }
