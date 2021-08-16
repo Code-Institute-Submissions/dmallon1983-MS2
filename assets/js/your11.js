9934d67bddbb28e083598eaf322d155657d16efc
dmallon1983 <D.mallon1983@hotmail.com>
2021-08-16 22:51:55 +0000

    created dropdown linked to array in JS file

--- assets/js/your11.js
+++ assets/js/your11.js
@@ -1,3 +1,28 @@
+let goalkeepers = ["Lev Yashin", "Gordon Banks", "Gianluigi Buffon", "Manuel Neuer", "Iker Casillas", "Edwin Van Der Sar"];
+
+let defenders = ["Cafu", "Roberto Carlos", "Ashley Cole", "Gary Neville", "Danny McGrain", "Philip Lahm", "Javier Zanetti", "Dani Alves", "Carlos Alberto", "Franz Beckenbeaur", "Franco Baresi", "Paolo Maldini", "Rio Ferdinand", "Nemanja Vidic", "John Terry", "Billy McNeill", "Virgil Van Dijk", "Fabio Cannavaro"];
+
+let midfielders = ["Xavi", "Andres Iniesta", "Roy Keane", "Zinedine Zidane", "Paul Scholes", "Patrick Viera", "Andrea Pirlo", "Claude Makalele", "Bobby Charlton", "Graeme Souness"];
+
+let forwards = ["Diego Maradona", "Pele", "Ronaldo", "Cristiano Ronaldo", "Lionel Messi", "Thierry Henry", "Ferenc Puskas", "Ronaldinho", "Robert Lewandowski", "Henrik Larsson"];
+
+var dropdown = document.getElementById("selectGoalKeeper");
+
+for (var i = 0; i < goalkeepers.length; i++) {
+    
+    dropdown[dropdown.length] = new Option(goalkeepers[i], goalkeepers[i]);
+}
+
+let dropdown = document.getElementsByClassName("selectDefender");
+
+for (let i = 0; i < defenders.length; i++) {
+    
+    dropdown[dropdown.length] = new Option(defenders[i], defenders[i]);
+}
+
+
+
+
 function sendMail(contactForm) {
     emailjs.send("gmail", "your_11", {
             "from_name": contactForm.name.value,
