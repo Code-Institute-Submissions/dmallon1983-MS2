8214caaadaa2d1c69725c088e09501cdda3ce757
dmallon1983 <D.mallon1983@hotmail.com>
2021-08-29 09:29:02 +0000

    form update to capture coorect data for emailJS

--- assets/js/your11.js
+++ assets/js/your11.js
@@ -108,8 +108,17 @@ function sendMail(contactForm) {
     emailjs.send("gmail", "your_11", {
             "from_name": contactForm.name.value,
             "from_email": contactForm.email_address.value,
-            "player_request": contactForm.legend.value,
-            "request_reason": contactForm.reason.value
+            "player_request_1": contactForm.goalKeeper.value,
+            "player_request_2": contactForm.rightBack.value,
+            "player_request_3": contactForm.rightCentreBack.value,
+            "player_request_4": contactForm.leftCentreBack.value,
+            "player_request_5": contactForm.leftBack.value,
+            "player_request_6": contactForm.rightMid.value,
+            "player_request_7": contactForm.leftMid.value,
+            "player_request_8": contactForm.attMid.value,
+            "player_request_9": contactForm.rightForward.value,
+            "player_request_10": contactForm.leftForward.value,
+            "player_request_11": contactForm.centreForward.value
         })
         .then(
             function (response) {
@@ -127,18 +136,9 @@ function sendMail(contactForm) {
 let form = document.getElementById("signup-form");
 form.addEventListener('submit', handleSubmit);
 
-// let errorMsg = document.getElementById("errors");
-
 function handleSubmit(event) {
-    event.preventDefault();
-    let legends = ["Lev Yashin", "Gordon Banks", "Gianluigi Buffon", "Manuel Neuer", "Iker Casillas", "Edwin Van Der Sar", "Cafu", "Roberto Carlos", "Ashley Cole", "Gary Neville", "Danny McGrain", "Philip Lahm", "Javier Zanetti", "Dani Alves", "Carlos Alberto", "Franz Beckenbeaur", "Franco Baresi", "Paolo Maldini", "Rio Ferdinand", "Nemanja Vidic", "John Terry", "Billy McNeill", "Virgil Van Dijk", "Fabio Cannavaro", "Xavi", "Andres Iniesta", "Roy Keane", "Zinedine Zidane", "Paul Scholes", "Patrick Viera", "Andrea Pirlo", "Claude Makalele", "Bobby Charlton", "Graeme Souness", "Diego Maradona", "Pele", "Ronaldo", "Cristiano Ronaldo", "Lionel Messi", "Thierry Henry", "Ferenc Puskas", "Ronaldinho", "Robert Lewandowski", "Henrik Larsson"];
-    let legend = document.getElementById("legend");
-    if (legends.includes(legend.text)) {
-        // errorMsg.innerHTML = `${legend.value} is already available for selection. If you would like another legend please try again.`;
-        alert(`${legend.text} is already available for selection. If you would like another legend please try again.`);
-    } else {
+        event.preventDefault();
         form.submit();
-        console.log('legends'); //need feedback. change to alert
+        alert("Congratulations! You have succesfully submitted your legends 11.\nPlease visit our social media sites at the bottom of the page to see if you've been successful!!!");
     }
-}
 
